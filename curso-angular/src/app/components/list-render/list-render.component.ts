f2edb509472278fc2acfe4c91991bdc286a09703
Andre Pessoa <andrepessoa182@hotmail.com>
2023-01-04 22:42:41 -0300

    Services do Angular

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Animal } from 'src/app/Animal';
+import { ListService } from 'src/app/services/list.service';
+
 
 @Component({
   selector: 'app-list-render',
@@ -15,7 +17,13 @@ animals: Animal [] =  [
 ]
 animalDetails = '';
 
+constructor(private listService: ListService){}
+
 showAge(animal: Animal) {
   this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+}
+removeAnimal(animal: Animal) {
+  console.log('Removendo animal...');
+ this.animals =  this.listService.remove(this.animals, animal);
 }
 }
